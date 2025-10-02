2ee5977d455c98c8a0abd3844b3aeb3500c156a1
jesse1231234 <jesse.thompson@colostate.edu>
2025-10-02 09:55:26 -0600

    Create main.ts

---
+++ apps/api/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+
+async function bootstrap() {
+const app = await NestFactory.create(AppModule, { logger: ['log','error','warn'] });
+const port = process.env.API_PORT || 4000;
+app.enableCors({ origin: '*' });
+await app.listen(port as number);
+console.log(`API listening on :${port}`);
+}
+bootstrap();
