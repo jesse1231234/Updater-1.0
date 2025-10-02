c6c7c87c8c4ca01a2616d79a54a9cc0a2219d90c
jesse1231234 <jesse.thompson@colostate.edu>
2025-10-02 15:21:45 -0600

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,12 +2,17 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
-const app = await NestFactory.create(AppModule, { logger: ['log','error','warn'] });
-const port = process.env.API_PORT || 4000;
-app.enableCors({ origin: '*' });
-await app.listen(port as number);
-console.log(`API listening on :${port}`);
+  const app = await NestFactory.create(AppModule, { logger: ['log','error','warn'] });
+
+  app.enableCors({ origin: '*' });
+
+  const port =
+    Number(process.env.NEST_PORT ?? process.env.API_PORT ?? 4000);
+
+  // IMPORTANT in containers:
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`API listening on :${port}`);
 }
 bootstrap();
