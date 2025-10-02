efca4ce72d0c4ed3cade35345bd81628112e7dd3
jesse1231234 <jesse.thompson@colostate.edu>
2025-10-02 15:49:34 -0600

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,14 +1,10 @@
-// apps/web/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
-      // If you do NOT call app.setGlobalPrefix('api') in Nest:
+      // You do NOT call app.setGlobalPrefix('api'), so use this:
       { source: '/api/:path*', destination: 'http://localhost:4000/:path*' },
-
-      // If you DO call app.setGlobalPrefix('api'), replace the above with:
-      // { source: '/api/:path*', destination: 'http://localhost:4000/api/:path*' },
     ];
   },
   output: 'standalone',
