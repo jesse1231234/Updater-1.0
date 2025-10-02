49671a954bb9bb35e8b4ffed6925d68b48d56984
jesse1231234 <jesse.thompson@colostate.edu>
2025-10-02 15:20:49 -0600

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,3 @@
+@@ -0,0 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      { source: '/api/:path*', destination: 'http://localhost:4000/:path*' },
-    ];
-  },
-  output: 'standalone',
-};
-module.exports = nextConfig;
+module.exports = { reactStrictMode: true };
